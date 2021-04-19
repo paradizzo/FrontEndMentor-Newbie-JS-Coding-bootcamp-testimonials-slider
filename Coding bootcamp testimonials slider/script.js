67e000e04e20ecbce301318f63987c50c3b5854a
paradizzo <80402261+paradizzo@users.noreply.github.com>
2021-04-19 12:58:38 -0300

    Add files via upload

---
+++ Coding bootcamp testimonials slider/script.js
@@ -0,0 +1,107 @@
+function GoFoward() {
+    var x = document.getElementById("image-man");
+    if (x.style.display === 'inline-block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'inline-block';
+    }
+
+} 
+function GoFoward2() {
+    var y = document.getElementById("image-woman");
+    if (y.style.display === 'none') {
+        y.style.display = 'inline-block';
+    } else {
+        y.style.display = 'none';
+    }
+}
+function GoBack() {
+    var x = document.getElementById("image-man");
+    if (x.style.display === 'none') {
+        x.style.display = 'inline-block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function GoBack2() {
+    var x = document.getElementById("image-woman");
+    if (x.style.display === 'inline-block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'inline-block';
+    }
+}
+
+/** TEXTO **/
+/** Durante a revisão percebi que coloquei "IMAGEFoward" sendo que se trata do título, pequeno erro mas vou manter dessa maneira, vai que quebra **/
+
+function ImageFoward() {
+    var x = document.getElementById("title-man");
+    if (x.style.display === 'inline-block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'inline-block';
+    }
+
+} 
+function ImageFoward2() {
+    var y = document.getElementById("title-woman");
+    if (y.style.display === 'none') {
+        y.style.display = 'inline-block';
+    } else {
+        y.style.display = 'none';
+    }
+}
+function ImageBack() {
+    var x = document.getElementById("title-man");
+    if (x.style.display === 'none') {
+        x.style.display = 'inline-block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function ImageBack2() {
+    var x = document.getElementById("title-woman");
+    if (x.style.display === 'inline-block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'inline-block';
+    }
+}
+/** Fim do script pro texto **/
+/* NOME E TRABALHO*/
+
+
+function NameFoward() {
+    var x = document.getElementById("name-man");
+    if (x.style.display === 'inline-block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'inline-block';
+    }
+
+} 
+function NameFoward2() {
+    var y = document.getElementById("name-woman");
+    if (y.style.display === 'none') {
+        y.style.display = 'inline-block';
+    } else {
+        y.style.display = 'none';
+    }
+}
+function NameBack() {
+    var x = document.getElementById("name-man");
+    if (x.style.display === 'none') {
+        x.style.display = 'inline-block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function NameBack2() {
+    var x = document.getElementById("name-woman");
+    if (x.style.display === 'inline-block') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'inline-block';
+    }
+}
